f329d7a75f4f719451db400fee998a4d613c5fcc
Farhad <farhaduneci@gmail.com>
2022-07-09 22:07:07 +0430

    ADD: about_us app

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,9 @@
+from accounts.models import User
+from django.shortcuts import render
+
+
 def about_us(request):
-    # View for about-us page
-    ...
+    users = User.objects.all()
+    return render(
+        request, "about_us.html", {"names": [user.get_full_name() for user in users]}
+    )
